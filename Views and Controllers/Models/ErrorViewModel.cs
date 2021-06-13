82af9dac145a3a1590b56de8de347e4f97c90ed3
Sneha Sil <snehasil413@gmail.com>
2021-06-13 15:37:06 -0500

    created project

---
+++ Views and Controllers/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Views_and_Controllers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
